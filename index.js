2ffa140bd8e1bf78a21aea95fda62c14d602ff4f
Glitch (srp) <none>
2018-10-02 22:06:19 +0000

    🌏🛀 Updated with Glitch

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const Discord   = require('discord.js');
+const client    = new Discord.Client();
+const settings  = require('settings.json');
+
+const prefix    = "-";
+client.on('message', message => {
+  if (message.author === client.user) return;
+  if (message.content.startsWith(prefix + 'ping')) {
+    message.channel.sendMessage('pong') 
+  }
+});
+
+client.login(settings.token);
